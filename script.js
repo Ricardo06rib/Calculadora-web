49a484334aef7fba49b4fdc01f4a36358417ea46
Ricardo Fernandes Ribeiro <ricardo.fernandes.ribeiro.06@gmail.com>
2025-06-08 16:55:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+var dp = "";
+
+function bot1() {
+
+    dp = dp + "1";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot2() {
+
+    dp = dp + "2";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot3() {
+
+    dp = dp + "3";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot4() {
+
+    dp = dp + "4";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot5() {
+
+    dp = dp + "5";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot6() {
+
+    dp = dp + "6";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot7() {
+
+    dp = dp + "7";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot8() {
+
+    dp = dp + "8";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot9() {
+
+    dp = dp + "9";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function bot0() {
+
+    dp = dp + "0";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function botmais() {
+
+    dp = dp + "+";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function botmenos() {
+
+    dp = dp + "-";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function botdiv() {
+
+    dp = dp + "/";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function botmult() {
+
+    dp = dp + "*";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function botigual() {
+
+    dp = eval(dp);
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
+
+function botC() {
+
+    dp = "";
+    console.log(dp);
+    document.getElementById("display").innerHTML= dp;
+
+}
